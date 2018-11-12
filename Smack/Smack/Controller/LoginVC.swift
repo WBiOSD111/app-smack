006e93e46558d03798c1650dfeb55f06a25c94e7
grav0211 <grav0211@algonquinlive.com>
2018-11-12 12:47:27 -0500

    Built Login view controller and added LoginVC.swift

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Alexandre Gravelle on 2018-11-12.
+//  Copyright © 2018 Alexandre Gravelle. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Alexandre Gravelle on 2018-11-12.
+//  Copyright © 2018 Alexandre Gravelle. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
