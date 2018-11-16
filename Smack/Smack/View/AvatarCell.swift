b2293cafdf5991c2166a2195e1d739590e3e6f59
grav0211 <grav0211@algonquinlive.com>
2018-11-15 19:39:06 -0500

    Added Avatar Picker View Controller

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Alexandre Gravelle on 2018-11-15.
+//  Copyright © 2018 Alexandre Gravelle. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
