4403bc7d6a54a1368744ec80e14dc9bb000676b6
grav0211 <grav0211@algonquinlive.com>
2018-11-17 13:58:29 -0500

    Added Channel model and find all channel functionality

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,22 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Alexandre Gravelle on 2018-11-17.
+//  Copyright © 2018 Alexandre Gravelle. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
+
+//struct Channel: Decodable {
+//    public private(set) var _id: String!
+//    public private(set) var name: String!
+//    public private(set) var description: String!
+//    public private(set) var __v: Int?
+//}
